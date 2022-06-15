862870c4ada468ffb1d1f1cee77aff2b432428e4
Juan Carlos <jccastillo.brito@gmail.com>
2022-06-15 12:19:06 -0400

    vista chat

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,5 @@
 const socket = io()
 
-//envio de chat
 function submitChat(event) 
 {
     
@@ -9,35 +8,23 @@ function submitChat(event)
     let email       = document.getElementById("email");
     let msj         = document.getElementById("mensaje");
     let hora        = moment().format("DD/MM/YYYY HH:mm:ss");
-    let nodo = {'email':email.value,'msj':msj.value,'created_at':hora}
+    let nodo        = [{'email':email.value,'msj':msj.value,'created_at':hora}]
 
     socket.emit('chat',nodo)
 
 
 }
 
-// envio de productos
-function submitForm(event)
-    {
-        event.preventDefault();
-        var titulo      = document.getElementById("title");
-        var precio      = document.getElementById("price");
-        var thumbnail   = document.getElementById("thumbnail");
-
-        let nodo = {'name': titulo.value,'price': precio.value,'thumbnail': thumbnail.value}
-        
-        socket.emit('notificacion',nodo)
-    }
 
     socket.on('mi mensaje', (data)=>{
 
         $('#table-body').html("");
+        console.log(data)
 
-        if(data && data.length)
-        {
-            data.forEach((element,index) => 
+        
+      /*  data.forEach((element,index) => 
             {
-                const item= `<tr> <td>${element.name}</td> <td>${element.price}</td> 
+                const item= `<tr> <td>${element.title}</td> <td>${element.price}</td> 
                     <td style="max-width:100px">
                         <img style="max-width:100px; !important" src="${element.thumbnail}">
                     </td> </tr>`;
@@ -47,29 +34,46 @@ function submitForm(event)
                 else
                 $('#table-body tr:last').after(item);
 
-            });
-        }
+            }); */
     })
 
     socket.on('chat_a_cliente', (data)=>{
-        
 
+        
         $('#lista_chat').html("");
         
-            if(data && data.length)
+       
+        data.forEach((element,index) => 
             {
-                data.forEach((element,index) => 
-                {
-                    let item= `<li><span style="color:blue;font-weight:700">${element.email}</span> <span>[${element.created_at}]</span> 
-                                <span style="color:green;font-style:italic">${element.msj}</span></li>`;
+                let item= `<li><span style="color:blue;font-weight:700">${element.email}</span> <span>[${element.created_at}]</span> 
+                            <span style="color:green;font-style:italic">${element.msj}</span></li>`;
 
-                    if(index ==0)
-                         $('#lista_chat').html(item);
-                    else
-                        $('#lista_chat li:last').after(item);
+                if(index ==0)
+                     $('#lista_chat').html(item);
+                else
+                    $('#lista_chat li:last').after(item);
 
-                }); 
-            }
-        
+            });
     })
 
+/*
+socket.on('mi mensaje', (data)=>{
+    console.log(data)
+})
+
+
+
+    btn.addEventListener('click', params=>{
+
+        let value = inputMensaje.value
+        socket.emit('mensaje',value)
+
+    })
+
+socket.on('mi mensaje',data=>{
+
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(data));
+    parrafo.appendChild(li);
+    
+}) */
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const pug = require('pug')
+
+let { Server : HttpServer }   = require('http')
+let { Server : IOServer }   = require('socket.io')
+
 const app = express()
+app.set('view engine','pug')
+app.use(express.static('./public'))
+
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
+
+
 
 const carroRouter = require('./src/routes/carroRouter')
 const productoRouter = require('./src/routes/productoRouter')
@@ -10,13 +22,33 @@ let port = process.env.PORT
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+app.use(express.static('./public'))
+
+app.get('/',(req,res)=>{
+  res.render('index',{root: __dirname})
+})
+
 
 app.use('/api/carrito',carroRouter)
 app.use('/api/carro',carroProducto)
 app.use('/api/productos',productoRouter)
 app.use('/api/mensajes',mensajeRouter)
 
-app.listen(port,()=>{
-  console.log(`Server corriendo en el puerto ${port}`)
+
+io.on('connection',(socket)=>{
+  console.log('Usuario Conectado')
+  socket.emit('mi mensaje','Hola nuevo usuario')
+
+  socket.on('notificacion',(info)=>{
+      console.log(info)
+  })
+
+  socket.on('mensaje',(info)=>{
+  io.sockets.emit('mi mensaje',info)
+
+
+  })
 })
 
+
+httpServer.listen(port)
